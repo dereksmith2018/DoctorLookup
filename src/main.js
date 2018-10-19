94750441f9fd05a10d1c852af6f5019f8e4c5243
Derek Smith <dereksmith@Dereks-MacBook-Air.local>
2018-10-19 16:51:35 -0700

    init commit

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+// const Promise = require('es6-promise').Promise;
+// const api_key = process.env.API_KEY;
+
+$(document).ready(function() {
+
+});
